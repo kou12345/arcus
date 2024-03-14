f556c28b9197ef74c138d6bb34b12282a4ec53c4
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-14 11:51:51 +0900

    add Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="mt-auto">
+      <hr />
+      <div className="flex justify-between mt-4 h-6">
+        <p className="text-sm">© 2021 Arcus</p>
+      </div>
+    </footer>
+  );
+};
