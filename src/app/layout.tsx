d3c6da414cfdc8569726edb3a35531e95b8e3463
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-14 11:54:30 +0900

    dark modeに変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +21,18 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <ClerkProvider>
-        <body className={inter.className}>{children}</body>
+        <body className={`${inter.className} min-h-screen flex flex-col p-2`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="flex-grow p-4">{children}</main>
+            <Footer />
+          </ThemeProvider>
+        </body>
       </ClerkProvider>
     </html>
   );
