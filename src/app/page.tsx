5843a4562edbd4182966e0e69110d4940ad5762a
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-14 12:19:14 +0900

    UI project一覧

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,97 @@
-import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default async function Home() {
+  const invoices = [
+    {
+      invoice: "INV001",
+      paymentStatus: "Paid",
+      totalAmount: "$250.00",
+      paymentMethod: "Credit Card",
+    },
+    {
+      invoice: "INV002",
+      paymentStatus: "Pending",
+      totalAmount: "$150.00",
+      paymentMethod: "PayPal",
+    },
+    {
+      invoice: "INV003",
+      paymentStatus: "Unpaid",
+      totalAmount: "$350.00",
+      paymentMethod: "Bank Transfer",
+    },
+    {
+      invoice: "INV004",
+      paymentStatus: "Paid",
+      totalAmount: "$450.00",
+      paymentMethod: "Credit Card",
+    },
+    {
+      invoice: "INV005",
+      paymentStatus: "Paid",
+      totalAmount: "$550.00",
+      paymentMethod: "PayPal",
+    },
+    {
+      invoice: "INV006",
+      paymentStatus: "Pending",
+      totalAmount: "$200.00",
+      paymentMethod: "Bank Transfer",
+    },
+    {
+      invoice: "INV007",
+      paymentStatus: "Unpaid",
+      totalAmount: "$300.00",
+      paymentMethod: "Credit Card",
+    },
+    {
+      invoice: "",
+      paymentStatus: "",
+      totalAmount: "",
+      paymentMethod: "",
+    },
+  ];
   return (
-    <div className="h-screen">
-      <UserButton />
-      {/* <div>{test.test}</div> */}
+    <div>
+      <div className="grid grid-cols-4 gap-4">
+        <p>Projects</p>
+        <Button className="col-start-4" size="sm">
+          new project
+        </Button>
+      </div>
+      <Table className="mt-2">
+        <TableCaption>A list of your recent projects.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="">Name</TableHead>
+            <TableHead></TableHead>
+            <TableHead></TableHead>
+            <TableHead className="text-right">Role</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoice) => (
+            <TableRow key={invoice.invoice}>
+              <TableCell className="font-medium">{invoice.invoice}</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+              <TableCell className="text-right">
+                {invoice.totalAmount}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
