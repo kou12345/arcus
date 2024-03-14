8a69b826a1a3e9ac67a36edf3e5dcfaaa9be98d5
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-15 07:23:04 +0900

    add newTaskRequest

--- src/types/type.ts
+++ src/types/type.ts
@@ -3,3 +3,8 @@ import { z } from "zod";
 export const newProjectRequest = z.object({
   projectName: z.string().min(1).max(48),
 })
+
+export const newTaskRequest = z.object({
+  projectId: z.string().uuid(),
+  taskName: z.string().min(1).max(60),
+})
