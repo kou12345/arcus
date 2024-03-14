5b8748f2c18e5cf55e23ed2af63a23b57d6f7a05
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-15 03:41:16 +0900

    columnにmapを追加

---
+++ prisma/migrations/20240314184013_fix_column_name/migration.sql
@@ -0,0 +1,72 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `project_roles` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `project_roles` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `projects` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `projects` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `task_comments` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `task_comments` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `task_priorities` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `task_priorities` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `task_statuses` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `task_statuses` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `task_users` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `task_users` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `tasks` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `tasks` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "project_roles" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "project_users" ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "projects" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "task_comments" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "task_priorities" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "task_statuses" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "task_users" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "tasks" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
