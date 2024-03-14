3f5caa3d62b50f1afc7deb8c40715c4f7d9b06a0
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-14 11:14:50 +0900

    trpcを消した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import { UserButton } from "@clerk/nextjs";
-import { serverApi } from "./_trpc/server-api";
 
 export default async function Home() {
-  const hello = await serverApi.hello.test();
   return (
     <div className="h-screen">
       <UserButton />
-      <div>{JSON.stringify(hello)}</div>
+      {/* <div>{test.test}</div> */}
     </div>
   );
 }
--- src/server/trpc.ts
+++
@@ -1,6 +0,0 @@
-import { initTRPC } from "@trpc/server";
-
-const t = initTRPC.create();
-
-export const {createCallerFactory, router} = t;
-export const publicProcedure = t.procedure;
