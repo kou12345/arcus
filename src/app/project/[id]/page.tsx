f67ce175f129143623c5356c10a8dfdbe253c7ea
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-15 14:10:09 +0900

    wip タスク一覧を実装

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,7 +1,78 @@
+import { NewTaskForm } from "@/components/NewTaskForm";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { getProjectById } from "@/server/actions/getProject";
+import { newTask } from "@/server/actions/newTask";
+import { db } from "@/server/db";
+import Link from "next/link";
+
 export default async function Page({ params }: { params: { id: string } }) {
+  // TODO ProjectIdのvalidation
+
+  const project = await getProjectById(params.id);
+
+  const newTaskWithProjectId = newTask.bind(null, params.id);
+
+  const tasks = await getTasksByProjectId(params.id);
+
   return (
     <div>
-      <div>{params.id}</div>
+      <div className="grid grid-cols-4 gap-4">
+        <div>{project.name}</div>
+
+        <NewTaskForm formAction={newTaskWithProjectId} />
+      </div>
+
+      <Table className="mt-2">
+        <TableCaption>A list of your recent projects.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="">Name</TableHead>
+            <TableHead></TableHead>
+            <TableHead>Deadline</TableHead>
+            <TableHead className="text-right">Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {tasks.map((task) => (
+            <TableRow key={task.id}>
+              <TableCell className="font-medium">
+                <Link href={`/project/${params.id}/task/${task.id}`}>
+                  {task.name}
+                </Link>
+              </TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+              <TableCell className="text-right">
+                {/* {task.dueDate ? "Due" : "Open"} */}
+                hoge
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
+
+const getTasksByProjectId = async (projectId: string) => {
+  try {
+    const tasks = await db.task.findMany({
+      where: {
+        projectId,
+      },
+    });
+
+    return tasks;
+  } catch (error) {
+    console.error(error);
+    throw new Error("ERROR: Failed to get tasks by project id");
+  }
+};
