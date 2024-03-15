e95eeadf578a3c0e3882119d166779720e62b7cd
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-16 03:04:56 +0900

    タスク内容を表示して、コメントのスタイルを修正

--- src/app/project/[id]/task/[taskId]/page.tsx
+++ src/app/project/[id]/task/[taskId]/page.tsx
@@ -1,15 +1,9 @@
 import { CommentForm } from "@/components/CommentForm";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { postComment } from "@/server/actions/postComment";
 import { db } from "@/server/db";
+import { clerkClient } from "@clerk/nextjs/server";
 
 export default async function Page({
   params,
@@ -18,20 +12,34 @@ export default async function Page({
 }) {
   const postCommentByTaskId = postComment.bind(null, params.taskId);
 
+  const task = await getTaskById(params.taskId);
   const comments = await getCommentsByTaskId(params.taskId);
 
   return (
     <div>
-      <div>{params.id}</div>
-      <div>{params.taskId}</div>
+      <div className=" h-40">
+        <p className=" text-xl">{task.name}</p>
+      </div>
+
+      <hr />
 
       {comments.map((comment) => (
-        <Card key={comment.id}>
+        <Card key={comment.id} className="my-2">
           <CardHeader>
-            <p>userName</p>
+            <div className="flex justify-between">
+              <div className="w-32 flex items-center justify-between">
+                <Avatar className="w-8 h-8 rounded-full">
+                  <AvatarImage src={comment.imageUrl} />
+                </Avatar>
+                <p>{comment.userName}</p>
+              </div>
+              <p className="text-sm text-muted-foreground">
+                {comment.updatedAt.toISOString()}
+              </p>
+            </div>
           </CardHeader>
           <CardContent>
-            <p>{comment.content}</p>
+            <p className="whitespace-pre-wrap">{comment.content}</p>
           </CardContent>
         </Card>
       ))}
@@ -41,6 +49,25 @@ export default async function Page({
   );
 }
 
+const getTaskById = async (id: string) => {
+  try {
+    const task = await db.task.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!task) {
+      throw new Error("Task not found");
+    }
+
+    return task;
+  } catch (error) {
+    console.error(error);
+    throw new Error("ERROR: Failed to get task");
+  }
+};
+
 const getCommentsByTaskId = async (taskId: string) => {
   try {
     const comments = await db.taskComment.findMany({
@@ -49,7 +76,18 @@ const getCommentsByTaskId = async (taskId: string) => {
       },
     });
 
-    return comments;
+    const user = await clerkClient.users.getUser(comments[0].userId);
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const commentsWithUser = comments.map((comment) => ({
+      ...comment,
+      userName: user.username,
+      imageUrl: user.imageUrl,
+    }));
+
+    return commentsWithUser;
   } catch (error) {
     console.error(error);
     throw new Error("ERROR: Failed to get comments");
