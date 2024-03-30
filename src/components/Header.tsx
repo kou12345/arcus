27e7d297508fed1e3afc3673669cc2781f045384
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-31 01:45:57 +0900

    projectへのlinkを追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,22 @@
+"use client";
+
 import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 import { Skeleton } from "./ui/skeleton";
+import { usePathname } from "next/navigation";
 
 export const Header = () => {
+  const pathname = usePathname();
+  const projectName = pathname.split("/")[2];
+
   return (
     <div>
       <div className="flex justify-between mb-4 h-6">
         <Link href="/" className="text-xl">
           Arcus
         </Link>
+        <Link href={`/project/${projectName}`}>{projectName}</Link>
         <ClerkLoading>
           <Skeleton className="h-8 w-8 rounded-full" />
         </ClerkLoading>
